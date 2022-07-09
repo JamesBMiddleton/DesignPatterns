20cc576de9475333ceabff3a4e250bdf8129e1ce
JamesBMiddleton <james.ben.middleton@gmail.com>
2022-07-09 13:44:23 +0100

    Setting up gh-pages.

--- scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-cd ..
-cmake -S . -B ./build
-cd build
-make
-ctest
-./src/DesignPatterns
-# remember to sudo chmod +x run.sh
--- test/first_test.cpp
+++
@@ -1,4 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-
-TEST_CASE("test") { REQUIRE(1 == 1); }
